c708c8bb840953171ba1847a8425b6f2d83b9dd0
Enes Hecan <nshecan@gmail.com>
2020-11-17 16:20:44 +0100

    polybar: exchange title and artist place for music

--- .config/polybar/scripts/music.sh
+++ .config/polybar/scripts/music.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$(playerctl status 2> /dev/null)" == "Playing" || "$(playerctl status 2> /dev/null)" == "Paused" ]]; then
-    playerctl --player=playerctld metadata --format "{{ title }} - {{ artist }}"
+    playerctl --player=playerctld metadata --format "{{ artist }} - {{ title }}"
 else
     echo ""
 fi
