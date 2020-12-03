e175e80c091ea65faee92e85c523f1a1a1c498aa
Enes Hecan <nshecan@gmail.com>
2020-12-03 14:16:25 +0100

    Minor fixes for volume script

--- .config/i3/scripts/volume.sh
+++ .config/i3/scripts/volume.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 OP=${1}
-ICON=notification-audio-volume-medium
 
 getSink() {
     pacmd list-sinks | awk '/index:/{i++} /* index:/{print i; exit}'
@@ -9,6 +8,17 @@ getSink() {
 
 getVolume() {
     pacmd list-sinks | awk '/^\svolume:/{i++} i=='$(getSink)'{print $5; exit}'
+}
+
+# Doesn't detect mute yet
+getIcon() {
+    CURRENT_SINK=$(pactl info | grep "Default Sink" | cut -f3 -d" ")
+    MUTE_STATUS=$(pactl list sources | grep -A 10 ${CURRENT_SINK} | grep "Mute:")
+    if [ "${MUTE_STATUS}" == "Mute: yes" ]; then
+        echo "notification-audio-volume-muted"
+    else
+        echo "notification-audio-volume-medium"
+    fi
 }
 
 if [ ${OP} == "inc" ]; then
@@ -22,4 +32,4 @@ elif [ ${OP} == "micmute" ]; then
 fi
 
 LEVEL=$(getVolume)
-notify-send -u low -i ${ICON} -h string:x-canonical-private-synchronous:anything ${LEVEL}
+notify-send -u low -i $(getIcon) -h string:x-canonical-private-synchronous:anything ${LEVEL}
