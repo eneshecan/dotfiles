64990746621ad28ea5305c553d698e922f1aebde
Enes Hecan <nshecan@gmail.com>
2021-06-07 10:46:23 +0200

    i3lock color script: Fix deprecated parameters

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -8,32 +8,32 @@ W='#660000DD'  # wrong
 V='#006600DD'  # verifying
 
 i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
+--insidever-color=$C   \
+--ringver-color=$V     \
 \
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
+--insidewrong-color=$C \
+--ringwrong-color=$W   \
 \
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
+--inside-color=$B      \
+--ring-color=$D        \
+--line-color=$B        \
+--separator-color=$D   \
 \
---verifcolor=$T       \
---wrongcolor=$T       \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$W       \
---bshlcolor=$W        \
+--verif-color=$T       \
+--wrong-color=$T       \
+--time-color=$T        \
+--date-color=$T        \
+--layout-color=$T      \
+--keyhl-color=$W       \
+--bshl-color=$W        \
 \
---screen 1            \
---blur 10             \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%a %d %b"  \
-# --keylayout 1         \
+--screen 1             \
+--blur 10              \
+--clock                \
+--indicator            \
+--time-str="%H:%M:%S"  \
+--date-str="%a %d %b"  \
+--keylayout 1          \
 
 # --veriftext="Drinking verification can..."
 # --wrongtext="Nope!"
