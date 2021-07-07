8258f3b4456347107020fc2d74d0d137e7a2fcbc
Enes Hecan <nshecan@gmail.com>
2021-07-08 01:22:31 +0200

    Switch to pipewire and use pipewire-pulse for now

--- .config/i3/scripts/volume.sh
+++ .config/i3/scripts/volume.sh
@@ -3,20 +3,17 @@
 op=${1}
 
 if [ "${op}" == "inc" ]; then
-    pactl set-sink-volume @DEFAULT_SINK@ +5%
+    pamixer --increase 5
 elif [ "${op}" == "dec" ]; then
-    pactl set-sink-volume @DEFAULT_SINK@ -5%
+    pamixer --decrease 5
 elif [ "${op}" == "mute" ]; then
-    pactl set-sink-mute @DEFAULT_SINK@ toggle
-elif [ "${op}" == "micmute" ]; then
-    pactl set-source-mute @DEFAULT_SOURCE@ toggle
+    pamixer --toggle-mute
 fi
 
-activeSink=$(pacmd list-sinks | awk '/* index:/{print $3}')
-level=$(($(pacmd list-sinks | grep -wA 15 'index: '${activeSink}'' | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')))
-muteStatus=$(pacmd list-sinks | grep -A 15 'index: '${activeSink}'' | awk '/muted/{ print $2}')
+level=$(pamixer --get-volume)
+muteStatus=$(pamixer --get-mute)
 icon="notification-audio-volume-medium"
-if [ "${muteStatus}" == "yes" ]; then
+if [ "${muteStatus}" == "true" ]; then
     level=0
     icon="notification-audio-volume-muted"
 elif [ ${level} -gt 70 ]; then
