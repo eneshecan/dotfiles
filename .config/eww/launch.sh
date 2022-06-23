4eb549bc53203c1a38fb3df62bf5cb526c714ebc
Enes Hecan <nshecan@gmail.com>
2022-06-23 14:54:20 +0200

    Various improvements for eww widgets

--- .config/eww/launch.sh
+++ .config/eww/launch.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-widget=$1
-file="$HOME/.cache/eww_launch_${widget}.xyz"
+window=$1
 eww="/usr/bin/eww"
 
-run_eww() {
-	${eww} open ${widget}
-}
-
-if [[ ! -f "$file" ]]; then
-	touch "$file"
-	run_eww
-else
-	${eww} close ${widget}
-	rm "$file"
-fi
-
+${eww} open --toggle ${window}
