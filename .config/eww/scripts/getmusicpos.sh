62758f46fbc7ebbc5f0aad6c658b7fc0faaf86ec
Enes Hecan <nshecan@gmail.com>
2022-07-12 15:19:54 +0200

    Minor fixes and improvements about drop shadows

--- .config/eww/scripts/getmusicpos.sh
+++ .config/eww/scripts/getmusicpos.sh
@@ -5,6 +5,11 @@ if ! playerctl position > /dev/null 2>&1; then
 	exit
 fi
 
+if ! playerctl metadata mpris:length > /dev/null 2>&1; then
+	echo "0"
+	exit
+fi
+
 position=$(echo "$(playerctl position) * 1000000" | bc)
 duration=$(playerctl metadata mpris:length)
 
--- .config/eww/scripts/setmusicpos.sh
+++ .config/eww/scripts/setmusicpos.sh
@@ -4,6 +4,11 @@ if ! playerctl position > /dev/null 2>&1; then
 	exit
 fi
 
+if ! playerctl metadata mpris:length > /dev/null 2>&1; then
+	echo "0"
+	exit
+fi
+
 threshold=20  # seconds
 pos_int=$(printf "%.0f" ${1})
 duration=$(playerctl metadata mpris:length)
