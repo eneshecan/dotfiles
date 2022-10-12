56666a5ac019c96aff0e1f61f90861b0b154e23c
Enes Hecan <nshecan@gmail.com>
2022-10-13 00:39:23 +0200

    Fix indentations

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -4,15 +4,15 @@ op=${1}
 name="VPN"
 
 if [ "${op}" == "toggle" ]; then
-	if test -d /proc/sys/net/ipv4/conf/tun0 ; then
-		nmcli connection down ${name} > /dev/null 2>&1
-	else
-		nmcli connection up ${name} > /dev/null 2>&1
-	fi
+    if test -d /proc/sys/net/ipv4/conf/tun0 ; then
+        nmcli connection down ${name} > /dev/null 2>&1
+    else
+        nmcli connection up ${name} > /dev/null 2>&1
+    fi
 elif [ "${op}" == "gettext" ]; then
     if test -d /proc/sys/net/ipv4/conf/tun0 ; then
-		echo "{\"text\": \"\", \"class\": \"connected\", \"tooltip\": \"Connected to ${name}\"}"
+        echo "{\"text\": \"\", \"class\": \"connected\", \"tooltip\": \"Connected to ${name}\"}"
     else
-    	echo "{\"text\": \"\", \"class\": \"disconnected\", \"tooltip\": \"Disconnected from ${name}\"}"
+        echo "{\"text\": \"\", \"class\": \"disconnected\", \"tooltip\": \"Disconnected from ${name}\"}"
     fi
 fi
