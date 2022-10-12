354a9bbaa307ee59cc2f7996c5b359d7aa5306d1
Enes Hecan <nshecan@gmail.com>
2022-10-13 00:33:34 +0200

    Waybar: Implement a custom VPN module and cleanup the config a bit

---
+++ .config/waybar/scripts/vpn.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+op=${1}
+name="VPN"
+
+if [ "${op}" == "toggle" ]; then
+	if test -d /proc/sys/net/ipv4/conf/tun0 ; then
+		nmcli connection down ${name} > /dev/null 2>&1
+	else
+		nmcli connection up ${name} > /dev/null 2>&1
+	fi
+elif [ "${op}" == "gettext" ]; then
+    if test -d /proc/sys/net/ipv4/conf/tun0 ; then
+		echo "{\"text\": \"\", \"class\": \"connected\", \"tooltip\": \"Connected to ${name}\"}"
+    else
+    	echo "{\"text\": \"\", \"class\": \"disconnected\", \"tooltip\": \"Disconnected from ${name}\"}"
+    fi
+fi
