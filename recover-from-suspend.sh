f08a897dba17a40886c43bf409fb261c088eb7d1
Xeco <nshecan@gmail.com>
2024-04-04 15:25:52 +0200

    Remove useless script

--- recover-from-suspend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 0b:00.0 USB controller [0c03]: ASMedia Technology Inc. ASM1042A USB 3.0 Host Controller [1b21:1142]
-SERIAL=$(lspci -Dd "1b21:1142" | awk '{ print $1 }')
-DRIVER="/sys/bus/pci/drivers/xhci_hcd"
-
-echo -n $SERIAL | sudo tee $DRIVER/unbind
-sleep 4
-echo -n $SERIAL | sudo tee $DRIVER/bind
