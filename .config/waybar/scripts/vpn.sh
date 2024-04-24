ae3352434809f63bfaa71a1ca39f7f19a259a611
Xeco <nshecan@gmail.com>
2024-04-24 14:52:23 +0200

    Use waybar mpris

--- .config/waybar/scripts/vpn.sh
+++ .config/waybar/scripts/vpn.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 op=${1}
-name="VPN"
+name="4-VPN-443udp"
 
 if [ "${op}" == "toggle" ]; then
     if test -d /proc/sys/net/ipv4/conf/tun0 ; then
-        nmcli connection down ${name} > /dev/null 2>&1
+        openvpn3 session-manage --disconnect --config ${name}.ovpn > /dev/null 2>&1
     else
-        nmcli connection up ${name} > /dev/null 2>&1
+        printf "username\npassword\n" | openvpn3 session-start --config ${name}.ovpn > /dev/null 2>&1
     fi
 elif [ "${op}" == "gettext" ]; then
     if test -d /proc/sys/net/ipv4/conf/tun0 ; then
